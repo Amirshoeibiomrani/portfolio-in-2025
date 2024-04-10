f6d3899223e3a882c6d83ec922a83e8f085b8c35
AmirShoeibiOmrani <amirtahaomrany@gmail.com>
2024-04-10 11:02:38 -0500

    made Logo component

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import logo from '../public/other/logo.svg'
 
- const Logo = () => {
+const Logo = () => {
   return (
-    <div>Logo</div>
+    <Link href="/">
+      <Image src={logo} width={54} height={54} priority alt='Logo' />
+      </Link>
   )
 }
 export default Logo
