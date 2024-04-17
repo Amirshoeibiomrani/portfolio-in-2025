04bc6a926c2c8a5f14ac3730235a0c38677ea3e8
AmirShoeibiOmrani <amirtahaomrany@gmail.com>
2024-04-17 12:38:42 -0500

    Hero Update 57

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+
+"use client"
+
+import Link from "next/link"
+import { Button } from "./ui/button"
+import {Download, Send} from "lucide-react"
+
+import {RiBriefcase4Fill, RiTeamFill, RiTodoFill, RiArrowDownSLine} from "react-icons/ri"
+
+// components
+import DevImg from "./DevImg"
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <section className="py-12 xl:py-24 h-[84vh] xl:pt-28 bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none" >
+        <div className="container mx-auto">
+            <div className="flex justify-between gap-x-8">
+                {/* text */}
+                <div className="flex max-w-[600px] flex-col justify-center mx-auto xl:mx-0 text-center xl:text-left">
+                    <div className=" text-sm uppercase font-semibold mb-4 text-primary tracking-[4px]">Web Developer</div>
+                    <h1 className="h1 mb-4">Hello, my name is Amir Omrani</h1>
+                    <p className="subtitle max-w-[490px] mx-auto xl:mx-0">
+                        Brief description with insights into myself, my vocational journey, and what i engage in professionally.
+                    </p>
+                    {/* buttons */}
+                    <div className="">
+                        <Link href='/contact'>
+                            <Button className="gap-x-2" >
+                                Contact me <Send size={18 } /> 
+                            </Button>
+                        </Link>
+                        <Button variant='secondary' className='gap-x-2' >
+                            Download CV 
+                            <Download size={18 } />
+                            </Button>
+                    </div>
+                </div>
+                {/* image */}
+                <div className="hidden xl:flex relative">image</div>
+            </div>
+            {/* icon */}
+            <div className="hidden md:flex absolute left-2/4 bottom-44 xl:bottom-1/2 animate-bounce">
+                <RiArrowDownSLine className="text-3xl text-primary" />
+            </div>
+        </div>
+        
+
+    </section>
   )
 }
 
