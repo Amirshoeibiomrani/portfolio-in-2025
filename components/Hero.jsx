f7ac0b508ffd4ed500edda817a135d8d15ddd5f1
AmirShoeibiOmrani <amirtahaomrany@gmail.com>
2024-05-11 11:20:27 -0500

    Fixed bug and  make badge 2

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -19,7 +19,7 @@ function Hero() {
             <div className="flex justify-between gap-x-8">
                 {/* text */}
                 <div className="flex max-w-[600px] flex-col justify-center mx-auto xl:mx-0 text-center xl:text-left">
-                    <div className=" text-sm uppercase font-semibold mb-4 text-primary tracking-[4px]">Web Developer</div>
+                    <div className=" text-sm uppercase font-semibold mb-4 text-primary tracking-[4px]">Front End Developer</div>
                     <h1 className="h1 mb-4">Hello, my name is Amir Omrani</h1>
                     <p className="subtitle max-w-[490px] mx-auto xl:mx-0">
                         Brief description with insights into myself, my vocational journey, and what i engage in professionally.
@@ -43,6 +43,10 @@ function Hero() {
                 <div className="hidden xl:flex relative">
                     {/* badge 1 */}
                     <Badge containerStyles='absolute top-[24%] -left-[5rem]' icon={<RiBriefcase4Fill />} endCountNum={3} badgeText='Years Of Experience' />
+                    {/* badge 2 */}
+                    <Badge containerStyles='absolute top-[80%] -left-[1rem]' icon={<RiTodoFill />} endCountNum={22}  badgeText='Finished Projects' />
+
+
                     <div className="bg-hero_shape2_light dark:bg-hero_shape2_dark w-[500px] h-[500px] bg-no-repeat absolute -top-1 -right-2 "></div>
                     <DevImg containerStyles="bg-hero_shape w-[510px] h-[462px] bg-no-repeat relative bg-bottom"
                     imgSrc="/hero/Developer.png" />
