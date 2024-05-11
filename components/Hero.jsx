aaba127a5c22cbe55bca14fac69d434cb8799ccc
AmirShoeibiOmrani <amirtahaomrany@gmail.com>
2024-05-11 11:24:15 -0500

    make badge 3 "23.22"

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -46,6 +46,9 @@ function Hero() {
                     {/* badge 2 */}
                     <Badge containerStyles='absolute top-[80%] -left-[1rem]' icon={<RiTodoFill />} endCountNum={22}  badgeText='Finished Projects' />
 
+   {/* badge 3 */}
+   <Badge containerStyles='absolute top-[55%] -right-8' icon={<RiTeamFill />} endCountNum={50}  badgeText='Happy Clients' />
+
 
                     <div className="bg-hero_shape2_light dark:bg-hero_shape2_dark w-[500px] h-[500px] bg-no-repeat absolute -top-1 -right-2 "></div>
                     <DevImg containerStyles="bg-hero_shape w-[510px] h-[462px] bg-no-repeat relative bg-bottom"
