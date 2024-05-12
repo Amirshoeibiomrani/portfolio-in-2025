53c64f9db634f6a12315e557926b7eada3a7b3a0
AmirShoeibiOmrani <amirtahaomrany@gmail.com>
2024-05-12 12:17:50 -0500

    MobileNav

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -1,8 +1,28 @@
+import { AlignJustify, Sheet } from 'lucide-react';
 import React from 'react'
+import { SheetContent, SheetTrigger } from './ui/sheet';
+import Logo from './Logo';
+import Navbar from './Navbar';
+import Socials from './Socials';
 
   const MobileNav = () => {
   return (
-    <div>MobileNav</div>
+    <Sheet>
+      <SheetTrigger asChild>
+        <AlignJustify className='cursor-pointer' />
+
+      </SheetTrigger>
+      <SheetContent>
+        <div >
+          <div>
+            <Logo />
+            <Navbar />
+            <Socials />
+            
+          </div>
+        </div>
+      </SheetContent>
+    </Sheet>
   )
 }
 export default  MobileNav;
