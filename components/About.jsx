6f462e43659169b86934dd78646001e8898660a0
AmirShoeibiOmrani <amirtahaomrany@gmail.com>
2024-05-17 15:12:03 -0500

    About Updating

--- components/About.jsx
+++ components/About.jsx
@@ -1,6 +1,34 @@
-import React from 'react'
+import{TabsList,TabTrigger,Tabs,TabsContent} from "@/components/ui/tabs"
+ import{User2,MailIcon,HomeIcon,PhoneCall,GraduationCap,Calendar,Briefcase} from "lucide-react"
+ 
 
-const About = () => {
+ const infoData=[
+    {
+        icon: <User2 size={20} />,
+        text:"Amir Shoeibi Omrani",
+    },
+    {
+        icon: <PhoneCall size={20} />,
+        text:"+12 345 6789",
+    },
+    {
+        icon: <MailIcon size={20} />,
+        text:"amirtahaomrany@gmail.com",
+    },
+    {
+        icon: <Calendar size={20} />,
+        text:"Born on 21 Mar,1995",
+    },
+    {
+        icon: <GraduationCap size={20} />,
+        text:"Bachelors on Computer Science",
+    },
+    {
+        icon: <User2 size={20} />,
+        text:"Florida, USA",
+    },
+ ]
+ const About = () => {
   return (
     <div>About</div>
   )
