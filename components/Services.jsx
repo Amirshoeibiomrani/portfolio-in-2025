492a76115bef0734595b223d356fa571c4044cfe
AmirShoeibiOmrani <amirtahaomrany@gmail.com>
2024-05-22 07:18:33 -0500

    updating

--- components/Services.jsx
+++ components/Services.jsx
@@ -5,17 +5,17 @@ const servicesData=[
     {
     icon: <GanttChartSquare size={72 } strokeWidth={0.8} />,
     title:'Web Design',
-    description:'orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    description:'orem Ipsum is simply dummy text of the printing and typesetting industry.',
 },
 {
     icon: <Blocks size={72 } strokeWidth={0.8} />,
     title:'Web Development',
-    description:'orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    description:'orem Ipsum is simply dummy text of the printing and typesetting industry.',
 },
 {
     icon: <Gem size={72 } strokeWidth={0.8} />,
     title:'App Development',
-    description:'orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    description:'orem Ipsum is simply dummy text of the printing and typesetting industry.',
 },
 ]
 
@@ -25,7 +25,19 @@ const Services = () => {
         <div className="container mx-auto">
             <h2 className='section-title mb-12 xl:mb-24 text-center mx-auto'>My Services</h2>
             {/* grid items */}
-            <div>grid items</div>
+            <div>
+        {servicesData.map((item,index)=>{
+            return(
+                <Card className="w-full max-w-[424px] h-[300px] flex flex-col pt-16 pb-10 justify-center items-center relative" key={index}>
+                    <CardHeader className="text-primary absolute -top-[60px]" >
+                        <div className="w-[140px] h-[80px] bg-pink-100 dark:bg-background flex justify-center items-center">
+                            {item.icon}
+                        </div>
+                    </CardHeader>
+                </Card>
+            )
+        })}
+            </div>
         </div>
     </section>
   )
